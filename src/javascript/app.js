177b08ce9fc49734e0f3f5743cb41d8d528989f1
Miguel Costa <user@MacBook-de-User.Home>
2025-06-28 00:00:48 +0100

    Melhorias no menu

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -1,7 +1,15 @@
 const hamburger = document.querySelector(".menu-hamburger")
 const navBar = document.querySelector(".menu-list")
+const navLink = document.querySelectorAll(".menu-link")
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active")
   navBar.classList.toggle("active")
+})
+
+navLink.forEach((link) => {
+  link.addEventListener("click", () => {
+    hamburger.classList.toggle("active")
+    navBar.classList.toggle("active")
+  })
 })
